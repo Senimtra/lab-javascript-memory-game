d9772c4aed8e585e6452e53cbf31e0e01c747b6c
Senimtra <senimtra@hotmail.com>
2021-07-05 16:09:27 +0200

    solve iteration 3: html/css interactions

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+// ########################################
+// ## Iteration 3: HTML/CSS Interactions ##
+// ########################################
+
 const cards = [
   { name: 'aquaman', img: 'aquaman.jpg' },
   { name: 'batman', img: 'batman.jpg' },
@@ -25,10 +29,16 @@ const cards = [
   { name: 'thor', img: 'thor.jpg' }
 ];
 
+// get the DOM score elements
+const scorePairsClickedElement = document.querySelector('#pairs-clicked');
+const scorePairsGuessedElement = document.querySelector('#pairs-guessed');
+const scoreHeadingElement = document.querySelector('#score h2');
+
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', (event) => {
   let html = '';
+  memoryGame.shuffleCards();
   memoryGame.cards.forEach((pic) => {
     html += `
       <div class="card" data-card-name="${pic.name}">
@@ -44,8 +54,34 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      // add class 'turned' to picked card
+      card.classList.toggle('turned');
+      // push card to pickedCards array
+      memoryGame.pickedCards.push(card);
+      // check if there are two cards in the array
+      if (memoryGame.pickedCards.length === 2) {
+        // increment pairsClicked and update score
+        memoryGame.pairsClicked++;
+        scorePairsClickedElement.innerText = memoryGame.pairsClicked;
+        // setTimeout to flip back all cards that are not guessed ('blocked') and not just picked
+        setTimeout(() => [...document.querySelectorAll('.card:not(.blocked)')]
+          .filter(n => !memoryGame.pickedCards.includes(n))
+          .forEach(card => card.classList.remove('turned')), 1000);
+        // check if the cards are the same
+        if (memoryGame.pickedCards[0]
+          .getAttribute('data-card-name') === memoryGame.pickedCards[1]
+            .getAttribute('data-card-name')) {
+          // increment pairsGuessed and update score
+          memoryGame.pairsGuessed++;
+          scorePairsGuessedElement.innerText = memoryGame.pairsGuessed;
+          // add class 'blocked' to both cards
+          memoryGame.pickedCards.forEach(n => n.classList.add('blocked'))
+          // check if all pairs are guessed + print msg
+          if (memoryGame.checkIfFinished()) scoreHeadingElement.innerText = 'YOU WON!';
+        };
+        // clear the pickedCards array
+        memoryGame.pickedCards = [];
+      }
     });
   });
 });
